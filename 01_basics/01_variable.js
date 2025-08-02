96431b9a29871e18827b65a43f67f7d416593792
Rajkishore Satpathy <rajkishoresatapathy2002@gmail.com>
2025-08-02 10:25:02 +0000

    updates

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,26 @@
+const accountId = 1234567
+let accountEmail = "raj123@gamil.com"
+var accountPass = "7735"
+accountCity = "Bhubaneswar"
+let accountState;
+// accountId=12345 
+accountEmail="ram23@gmail.com"
+accountPass="7781"
+accountCity="puri"
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPass);
+// console.log(accountCity);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+// ---------------------------------NOTES----------------------------
+// <- This is called Single line comment
+/* <- This is called 
+    Multi-Line Comment*/
+// -> comments are not executed by the interpreter
+/*
+-> Prefer not to use Var keyword
+because of issue in block scope and functional scope
+*/
+
+
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Rajesh")
+
+
+let name = "Rajesh"//string
+let age = 18//int
+let isLoggedIn = false//bool
+let state; //undefined
+
+// number => (range upto)2 to power 53
+// bigint => greaterthen 2 to the power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
